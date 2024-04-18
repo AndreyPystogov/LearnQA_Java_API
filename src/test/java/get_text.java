f3dced17a5818c7952db09ff37fd46f757ade720
AndreyPystogov <pystogoov@gmail.com>
2024-04-18 22:55:17 +0500

    15 symbols

--- src/test/java/get_text.java
+++ src/test/java/get_text.java
@@ -2,13 +2,17 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class get_text {
     @Test
     public void getTextt(){
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
                 .andReturn();
-        response.prettyPrint();
+        System.out.println(response.asString().length());
+        assertTrue(response.asString().length() >15, "Message has less than 15 symbols");
 
 
     }
